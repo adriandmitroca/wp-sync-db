e6221cf3ea71af9f3f7652ee48620ed93a318e2e
Adrian Dmitroca <dmitroca.adrian@gmail.com>
2020-09-10 10:56:07 +0200

    php 7.4 compatibility

--- class/wpsdb.php
+++ class/wpsdb.php
@@ -1230,7 +1230,7 @@ class WPSDB extends WPSDB_Base {
 		);
 		$return = array();
 		foreach( $results as $results ) {
-			$return[$results['table_name']] = ( $results['TABLE_ROWS'] == 0 ? 1 : $results['TABLE_ROWS'] );
+			$return[$results['TABLE_NAME']] = ( $results['TABLE_ROWS'] == 0 ? 1 : $results['TABLE_ROWS'] );
 		}
 		return $return;
 	}
